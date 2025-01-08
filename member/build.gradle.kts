ae44a9a51b5c9b5db37c17f18a7074389e7fad75
GiJung <101462387+GiJungPark@users.noreply.github.com>
2025-01-08 19:46:40 +0900

    chore: Spring JPA 의존성 추가

---
+++ member/build.gradle.kts
@@ -0,0 +1,9 @@
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+}
