4caf9aa0111aae02de2bc901a6ddd18dc151a75d
GiJung <101462387+GiJungPark@users.noreply.github.com>
2025-01-08 21:45:07 +0900

    feat: Memver Id 생성 Generator 작성

---
+++ member/src/main/kotlin/io/livestreaming/api/member/util/id/MemberIdGenerator.kt
@@ -0,0 +1,14 @@
+package io.livestreaming.api.member.util.id
+
+import io.livestreaming.api.member.domain.MemberId
+import io.livestreaming.api.member.util.uuid.UuidProvider
+import org.springframework.stereotype.Component
+
+@Component
+class MemberIdGenerator(
+    private val uuidProvider: UuidProvider,
+) : IdGenerator<MemberId> {
+    override fun generate(): MemberId {
+        return MemberId.of(uuidProvider.generate())
+    }
+}
