f8756f9df29a4c4dbfeb49b940e9851ea10f51c8
GiJungPark <weeds2577@gmail.com>
2025-01-29 01:42:54 +0900

    feat: 코인 구매 API 작성

--- commerce/src/main/kotlin/io/livestreaming/api/commerce/presentation/web/CommerceController.kt
+++ commerce/src/main/kotlin/io/livestreaming/api/commerce/presentation/web/CommerceController.kt
@@ -1,10 +1,22 @@
 package io.livestreaming.api.commerce.presentation.web
 
+import io.livestreaming.api.commerce.application.port.`in`.PurchaseCoinCommand
+import io.livestreaming.api.commerce.application.port.`in`.PurchaseCoinUseCase
+import io.livestreaming.api.commerce.presentation.web.request.PurchaseCoinRequest
 import org.springframework.web.bind.annotation.*
 
-@RestController("/commerce")
-class CoinController(
-
+@RestController
+class CommerceController(
+    private val purchaseCoinUseCase: PurchaseCoinUseCase
 ) {
+    @PostMapping("/commerce/coin/purchase")
+    fun purchaseCoin(@RequestBody request: PurchaseCoinRequest) {
+        val command = PurchaseCoinCommand.of(
+            memberId = request.memberId,
+            quantity = request.quantity,
+        )
+
+        purchaseCoinUseCase.purchase(command)
+    }
 
 }
