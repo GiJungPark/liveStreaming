7c8861106f80d4502201ce165f34b43550a0b710
GiJungPark <weeds2577@gmail.com>
2025-01-28 14:14:18 +0900

    feat: ProductCode 초기 데이터를 삽입하도록 구현

---
+++ commerce/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- DEFAULT PRODUCT_CODE INSERT --
+INSERT INTO product_code(id, name, description)
+VALUES (1, "COIN001", "스트리머에게 후원할 수 있는 상품");
+INSERT INTO product_code(id, name, description)
+VALUES (2, "SUB001", "스트리머의 채널을 구독할 수 있는 상품");
+INSERT INTO product_code(id, name, description)
+VALUES (3, "PASS001", "모든 채널의 광고를 스킵할 수 있는 상품");
+
