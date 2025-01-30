7357c48d5cba5e685ecd536a7450ea80da7d6d8e
GiJungPark <weeds2577@gmail.com>
2025-01-30 18:41:43 +0900

    remove: 상품 도메인을 제거

--- commerce/src/main/kotlin/io/livestreaming/api/commerce/infrastructure/ProductJpaRepository.kt
+++
@@ -1,7 +0,0 @@
-package io.livestreaming.api.commerce.infrastructure
-
-import io.livestreaming.api.commerce.domain.Product
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ProductJpaRepository : JpaRepository<Product, Long> {
-}
