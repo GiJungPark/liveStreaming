0230c718e544f80e1be6490ecb2ad9cb874ca993
GiJungPark <weeds2577@gmail.com>
2025-02-09 22:24:43 +0900

    chore: 초기화 data 코인 잔액을 널널하게 수정

--- commerce/src/main/resources/data.sql
+++ commerce/src/main/resources/data.sql
@@ -2,7 +2,13 @@
 
 -- MEMBER INFO FOR TEST --
 INSERT INTO member_coin_balance(member_id, amount)
-VALUES("testId", 1000);
+VALUES("testId1", 1000000);
+INSERT INTO member_coin_balance(member_id, amount)
+VALUES("testId2", 1000000);
+INSERT INTO member_coin_balance(member_id, amount)
+VALUES("testId3", 1000000);
+INSERT INTO member_coin_balance(member_id, amount)
+VALUES("testId4", 1000000);
 
 INSERT INTO channel_coin_balance(channel_id, amount)
-VALUES("testChannelId", 1000);
+VALUES("testChannelId", 1000000);
