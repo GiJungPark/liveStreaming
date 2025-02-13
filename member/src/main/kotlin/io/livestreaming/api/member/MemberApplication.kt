42d9ebdf7b3fe3c6caaa03044f5a59f5230d4256
GiJungPark <weeds2577@gmail.com>
2025-02-13 12:38:13 +0900

    fix: Member 패키지를 스캔할 수 있도록 수정

--- member/src/main/kotlin/io/livestreaming/api/member/MemberApplication.kt
+++ member/src/main/kotlin/io/livestreaming/api/member/MemberApplication.kt
@@ -3,9 +3,9 @@ package io.livestreaming.api.member
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["io.livestreaming.api.common"])
-class UserApplication
+@SpringBootApplication(scanBasePackages = ["io.livestreaming.api.member", "io.livestreaming.api.common"])
+class MemberApplication
 
 fun main(args: Array<String>) {
-    runApplication<UserApplication>(*args)
+    runApplication<MemberApplication>(*args)
 }
